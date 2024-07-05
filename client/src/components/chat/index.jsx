7228afb024774e0ccadbd4e55826776196868825
Nitesh Kumar Ojha <nitesh303607@gmail.com>
2024-07-05 14:51:01 +0530

    chat application and react engine setup

---
+++ client/src/components/chat/index.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+  useMultiChatLogic,
+  MultiChatSocket,
+  MultiChatWindow,
+} from "react-chat-engine-advanced";
+
+import Header from "@/components/customHeader";
+
+function Chat() {
+  const chatProps = useMultiChatLogic(
+    import.meta.env.VITE_PROJECT_ID,
+    "testuser",
+    "test"
+  );
+
+  return (
+    <div style={{ flexBasis: "100%" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow
+        {...chatProps}
+        style={{ height: "100vh" }}
+        renderChatHeader={(chat) => <Header chat={chat} />}
+      />
+    </div>
+  );
+}
+
+export default Chat;
